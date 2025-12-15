4f66c8f13ea0ba84c7cb10def1b921e33d4d9228
Chenglin Luo <chenglinluoo@gmail.com>
2025-12-15 21:43:45 +0800

    First commit: Initialize project

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello, Git Main Branch!" << std::endl;
+    std::cout << "This is a new feature from dev branch." << std::endl;
+    return 0;
+}
